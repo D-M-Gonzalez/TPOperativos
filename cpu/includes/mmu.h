f836ea9a5df465cccbb8a2d64217c2e23b823ab1
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-06-16 23:27:50 +0000

    Agrego buscar_segmento.

--- cpu/includes/mmu.h
+++ cpu/includes/mmu.h
@@ -12,6 +12,7 @@ extern int tam_max_segmento;
 extern t_log *logger;
 
 void traducir_direccion(char *param, tabla_segmentos_t *tabla_segmentos);
+segmento_t* buscar_segmento(int num_segmento, t_list *segmentos);
 int obtener_direccion_base(int num_segmento, tabla_segmentos_t *tabla_segmentos);
 
 #endif /* MMU_H_ */
--- cpu/src/mmu.c
+++ cpu/src/mmu.c
@@ -8,35 +8,49 @@ void traducir_direccion(char *param, tabla_segmentos_t *tabla_segmentos)
 	int num_segmento = floor(direccion_logica / tam_max_segmento);
 	int desplazamiento_segmento = direccion_logica % tam_max_segmento;
 	int direccion_base_segmento = obtener_direccion_base(num_segmento, tabla_segmentos);
+	//int direccion_limite_segmento = obtener_direccion_limite(num_segmento, tabla_segmentos);
 
 	direccion_fisica = direccion_base_segmento + desplazamiento_segmento;
 
 	log_info(logger, "Direccion fisica: %d", direccion_fisica);
 
-	//if(desplazamiento_segmento + tamanio_direccion > tamanio_segmento) segmentation fault
+	/*if (direccion_base_segmento + desplazamiento_segmento > direccion_limite_segmento)
+	{
+		log_info(logger, "SEG_FAULT");
+	}*/
 
 }
 
-int obtener_direccion_base(int num_segmento, tabla_segmentos_t *tabla_segmentos)
+segmento_t* buscar_segmento(int num_segmento, t_list *segmentos)
 {
-	int direccion_base = -1;
-	t_list *segmentos = tabla_segmentos->segmentos;
-
 	for (int i = 0; i < list_size(segmentos); i++)
 	{
 		segmento_t *segmento = (segmento_t*) list_get(segmentos, i);
 
 		if (segmento->ids == num_segmento)
 		{
-			direccion_base = segmento->direccion_base;
-			break;
+			return segmento;
 		}
 	}
 
-	if(direccion_base == -1)
+	return NULL;
+}
+
+int obtener_direccion_base(int num_segmento, tabla_segmentos_t *tabla_segmentos)
+{
+	segmento_t* segmento = buscar_segmento(num_segmento, tabla_segmentos->segmentos);
+
+	if(segmento != NULL)
+	{
+		return segmento->direccion_base;
+	}
+	else
 	{
 		log_info(logger, "El numero de segmento no pertenece a la tabla de segmentos del proceso");
+		return -1;
 	}
-
-	return direccion_base;
 }
+
+/*int obtener_direccion_limite(int num_segmento, tabla_segmentos_t *tabla_segmentos)
+{
+}*/
